e035f60d0bfb8ee8e9a70c47e1067b8049f62cd2
Jim Crossley <jim@crossleys.org>
2020-02-06 20:00:12 -0500

    Reflect new org name in imports

--- patch/patch_test.go
+++ patch/patch_test.go
@@ -7,7 +7,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/yaml"
 
-	. "github.com/jcrossley3/manifestival/patch"
+	. "github.com/manifestival/manifestival/patch"
 )
 
 type updateChangedTestCases struct {
